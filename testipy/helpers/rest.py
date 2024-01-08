07de531e0306edb789488a2b6f1c6f3ef2d3ab34
Pedro Nunes <pjn2work@gmail.com>
2024-01-08 12:57:21 +0000

    add ddt tests

--- testipy/helpers/rest.py
+++ testipy/helpers/rest.py
@@ -3,33 +3,33 @@ import inspect
 from requests import Response
 
 
-from testipy.helpers.handle_assertions import assert_status_code, ExpectedError
+from testipy.helpers.handle_assertions import assert_status_code, ExpectedError, assert_data
 
 
 def get_response():
-    return handle_response.body
+    return handle_http_response.body
 
 
 def get_raw_response() -> Response:
-    return handle_response.raw
+    return handle_http_response.raw
 
 
 def get_status_code():
-    return handle_response.raw.status_code
+    return handle_http_response.raw.status_code
 
 
 def get_headers():
-    return handle_response.raw.headers
+    return handle_http_response.raw.headers
 
 
-def handle_response(_func=None, *, expected_type=None):
-    handle_response.raw = None
-    handle_response.body = None
+def handle_http_response(_func=None, *, expected_type=None):
+    handle_http_response.raw = None
+    handle_http_response.body = None
 
     def decorator(func):
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            handle_response.raw = response = func(*args, **kwargs)
+        def handle_http_response_wrapper(*args, **kwargs):
+            handle_http_response.raw = response = func(*args, **kwargs)
 
             # get default parameters from func and update them
             default_kwargs = dict(inspect.signature(func).parameters)
@@ -41,21 +41,37 @@ def handle_response(_func=None, *, expected_type=None):
             if default_kwargs.get("expected_status_code") == default_kwargs.get("ok"):
                 if expected_type is None:
                     return None
-                handle_response.body = response = response.text if expected_type == str else response.json()
+                handle_http_response.body = response = response.text if expected_type == str else response.json()
+
                 assert isinstance(response, expected_type), f"must receive a {type(expected_type)} not a {type(response)}"
+                if expected_response:= default_kwargs.get("expected_response"):
+                    assert_data(expected_values=expected_response, response=response)
+
                 return response
             else:
                 if response.status_code == 204:
-                    assert response.text == "", "response should be empty!"
+                    handle_http_response.body = response.text
+                    assert response.text == "", "handle_http_response: status_code=204, response should be empty!"
+                else:
+                    if expected_response := default_kwargs.get("expected_response"):
+                        if isinstance(expected_response, (dict, list)):
+                            try:
+                                handle_http_response.body = received_response = response.json()
+                            except:
+                                raise AssertionError("handle_http_response: error message not a JSON!")
+                        else:
+                            handle_http_response.body = received_response = response.text
 
-                if expected_type and expected_type != str:
-                    try:
-                        handle_response.body = response.json()
-                    except:
-                        raise AssertionError("error message response, not a JSON!")
+                        assert_data(expected_values=expected_response, response=received_response)
+                    else:
+                        try:
+                            handle_http_response.body = response.json()
+                        except:
+                            handle_http_response.body = response.text
+
                 raise ExpectedError(f"designed to fail with {response.status_code}")
 
-        return wrapper
+        return handle_http_response_wrapper
 
     if _func is None:
         return decorator    # under a class instance
---
+++ testipy/tests/demo/ddt/pet_store.py
@@ -0,0 +1,82 @@
+import requests
+
+from typing import Dict
+
+from testipy.reporter import ReportManager
+from testipy.reporter.report_base import TestDetails
+from testipy.helpers import prettify
+from testipy.helpers.data_driven_testing import ExecutionToolbox, SafeTry
+from testipy.helpers.handle_assertions import ExpectedError
+from testipy.helpers.rest import handle_http_response
+
+
+class Toolbox(ExecutionToolbox):
+
+    def execute(self, rm: ReportManager, current_test: TestDetails, exec_method, usecase: Dict, usecase_name: str, st: SafeTry):
+        exec_method = f"self.{usecase['_exec_method_']}(rm=rm, current_test=current_test, usecase=usecase, usecase_name=usecase_name, st=st)"
+        eval(exec_method)
+
+    def clear_last_execution(self):
+        handle_http_response.body = handle_http_response.raw = ""
+
+    @handle_http_response(expected_type=dict)
+    def _get_as_dict(self, url: str = "", timeout: int = 5, expected_status_code: int = 200, ok: int = 200, expected_response=None) -> Dict:
+        return requests.get(url, headers={"accept": "application/json"}, timeout=timeout)
+
+    @handle_http_response(expected_type=dict)
+    def _post_as_dict(self, url: str = "", data: Dict = None, timeout: int = 5, expected_status_code: int = 200, ok: int = 200, expected_response=None) -> Dict:
+        return requests.post(url, json=data, headers={"Content-Type": "application/json; charset=utf-8", "accept": "application/json"}, timeout=timeout)
+
+    @handle_http_response(expected_type=dict)
+    def _put_as_dict(self, url: str = "", data: Dict = None, timeout: int = 5, expected_status_code: int = 200, ok: int = 200, expected_response=None) -> Dict:
+        return requests.put(url, json=data, headers={"Content-Type": "application/json; charset=utf-8", "accept": "application/json"}, timeout=timeout)
+
+    @handle_http_response(expected_type=str)
+    def _delete_as_str(self, url: str = "", timeout: int = 5, expected_status_code: int = 200, ok: int = 200, expected_response=None) -> Dict:
+        return requests.delete(url, headers={"accept": "application/json"}, timeout=timeout)
+
+    def post_pet(self, rm: ReportManager, current_test: TestDetails, usecase: Dict, usecase_name: str, st: SafeTry, **kwargs):
+        url = f"https://petstore3.swagger.io/api/v3/pet"
+        try:
+            response = self._post_as_dict(url, usecase["param"], **usecase["control"], expected_response=usecase.get("_expected_response_"))
+            rm.testInfo(current_test, f"{usecase_name} - POST {url} - received payload:\n" + prettify(response, as_yaml=False))
+        except Exception as ex:
+            self._show_expected_payload_vs_received(rm, current_test, usecase, usecase_name, url, "POST", ex)
+            raise
+
+    def get_pet(self, rm: ReportManager, current_test: TestDetails, usecase: Dict, usecase_name: str, **kwargs):
+        url = f"https://petstore3.swagger.io/api/v3/pet/" + str(usecase["param"])
+        try:
+            response = self._get_as_dict(url, **usecase["control"], expected_response=usecase.get("_expected_response_"))
+            rm.testInfo(current_test, f"{usecase_name} - GET {url} - received payload:\n" + prettify(response, as_yaml=False))
+        except Exception as ex:
+            self._show_expected_payload_vs_received(rm, current_test, usecase, usecase_name, url, "GET", ex)
+            raise
+
+    def put_pet(self, rm: ReportManager, current_test: TestDetails, usecase: Dict, usecase_name: str, **kwargs):
+        url = f"https://petstore3.swagger.io/api/v3/pet"
+        try:
+            response = self._put_as_dict(url, usecase["param"], **usecase["control"], expected_response=usecase.get("_expected_response_"))
+            rm.testInfo(current_test, f"{usecase_name} - PUT {url} - received payload:\n" + prettify(response, as_yaml=False))
+        except Exception as ex:
+            self._show_expected_payload_vs_received(rm, current_test, usecase, usecase_name, url, "PUT", ex)
+            raise
+
+    def delete_pet(self, rm: ReportManager, current_test: TestDetails, usecase: Dict, usecase_name: str, **kwargs):
+        url = f"https://petstore3.swagger.io/api/v3/pet/" + str(usecase["param"])
+        try:
+            response = self._delete_as_str(url, **usecase["control"], expected_response=usecase.get("_expected_response_"))
+            rm.testInfo(current_test, f"{usecase_name} - DELETE {url} - received payload:\n" + prettify(response, as_yaml=False))
+        except Exception as ex:
+            self._show_expected_payload_vs_received(rm, current_test, usecase, usecase_name, url, "DELETE", ex)
+            raise
+
+    def _show_expected_payload_vs_received(self, rm: ReportManager, current_test: TestDetails, usecase: Dict, usecase_name: str, url: str, http_method: str, ex: Exception):
+        rm.testInfo(current_test,
+                    info=f"{usecase_name} - {http_method} {url} - received Error payload:\n" +
+                        prettify(handle_http_response.body, as_yaml=False),
+                    level="DEBUG" if isinstance(ex, ExpectedError) else "ERROR")
+        if expected_response := usecase.get("_expected_response_"):
+            rm.testInfo(current_test,
+                        info=f"{usecase_name} - expected payload:\n{prettify(expected_response, as_yaml=False)}",
+                        level="DEBUG")
