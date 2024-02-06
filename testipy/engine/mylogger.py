f1e531859d9ef7c87212fdfeb46828009d93e89a
Pedro Nunes <58593420+pjn2work@users.noreply.github.com>
2024-02-06 11:58:26 +0000

    Fix logger (#19)
    * fix to be compatible with v3.9
    
    * fix to be compatible with v3.9

--- testipy/engine/mylogger.py
+++ testipy/engine/mylogger.py
@@ -1,20 +1,22 @@
 import logging
+from typing import Union, Dict
 from datetime import datetime
 
 
 class MyScreenFormatter(logging.Formatter):
-    def __init__(self, *, fmt_keys: dict[str, str] | None = None):
+    def __init__(self, *, fmt_keys: Union[Dict[str, str], None] = None):
         super().__init__()
         self.fmt_keys = fmt_keys if fmt_keys is not None else {}
 
     def format(self, record: logging.LogRecord) -> str:
         ts = str(datetime.fromtimestamp(record.created))
-        message = f"{ts} {record.levelname} {record.getMessage().split("\n")[0]}"
+        message = record.getMessage().split("\n")[0]
+        message = f"{ts} {record.levelname} {message}"
         return message
 
 
 class MyFileFormatter(logging.Formatter):
-    def __init__(self, *, fmt_keys: dict[str, str] | None = None):
+    def __init__(self, *, fmt_keys: Union[Dict[str, str], None] = None):
         super().__init__()
         self.fmt_keys = fmt_keys if fmt_keys is not None else {}
 
